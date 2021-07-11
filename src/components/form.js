32c2cb424ac02a88a53cd6d85a2f7f98b5fbdda0
hadeel480 <77208260+hadeel480@users.noreply.github.com>
2021-07-12 02:59:50 +0300

    Add files via upload
    react application card

---
+++ src/components/form.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+let Form=()=>{
+return(<h1>hi react</h1>)
+
+}
+
+
+export default Form
---
+++ src/components/list.js
@@ -0,0 +1,13 @@
+import React ,{component}from 'react'
+
+
+class List extends component{
+
+
+    render(){
+
+        return(<hi>hiii</hi>)
+    }
+}
+
+export default List;
